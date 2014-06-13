61f503c4b87e7514a6e7b6292878627481331919
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-13 14:36:05 -0700

    Added gurobi

---
+++ trajopt/src/gurobi563/linux64/docs/refman/images.pl
@@ -0,0 +1,260 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/x_i;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img27.png"
+ ALT="$x_i$">|; 
+
+$key = q/Bx=A_j;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="66" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$Bx=A_j$">|; 
+
+$key = q/x_j;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="20" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="$x_j$">|; 
+
+$key = q/a^Ty-z;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="58" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img35.png"
+ ALT="$a^Ty - z$">|; 
+
+$key = q/z;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$z$">|; 
+
+$key = q/B^T;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="27" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$B^T$">|; 
+
+$key = q/c;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img36.png"
+ ALT="$c$">|; 
+
+$key = q/a^Txleb;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="60" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img30.png"
+ ALT="$a^Tx \le b$">|; 
+
+$key = q/x^TQx+c^Tx+mathrm{alpha};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="148" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="$x^TQx + c^Tx + \mathrm{alpha}$">|; 
+
+$key = q/B^{-1};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="34" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$B^{-1}$">|; 
+
+$key = q/2x0^2+x0*x1+x1^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="149" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$2 x0^2 + x0 * x1 +
+x1^2$">|; 
+
+$key = q/a^Tx+s=b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="87" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img31.png"
+ ALT="$a^Tx + s = b$">|; 
+
+$key = q/x_k;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="21" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img25.png"
+ ALT="$x_k$">|; 
+
+$key = q/Ax=b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="53" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="$Ax = b$">|; 
+
+$key = q/x^TQx+q^Txleb;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="119" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$x^TQx + q^Tx \le b$">|; 
+
+$key = q/A_j;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="23" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="$A_j$">|; 
+
+$key = q/10X01^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="58" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img39.png"
+ ALT="$10 X01^2$">|; 
+
+$key = q/y;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$y$">|; 
+
+$key = q/2^{31}-1;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="53" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img37.png"
+ ALT="$2^{31}-1$">|; 
+
+$key = q/i;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="10" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$i$">|; 
+
+$key = q/B;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$B$">|; 
+
+$key = q/x^Txley^{2};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="69" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$x^Tx \le y^{2}$">|; 
+
+$key = q/a^Tygec;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="59" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img33.png"
+ ALT="$a^Ty \ge c$">|; 
+
+$key = q/Q;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$Q$">|; 
+
+$key = q/B^Tx=b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="64" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$B^Tx=b$">|; 
+
+$key = q/elllexleu;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="71" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="$\ell \le x \le u$">|; 
+
+$key = q/2X02^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img41.png"
+ ALT="$2 X02^2$">|; 
+
+$key = q/4X01*X02;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="88" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img40.png"
+ ALT="$4 X01*X02$">|; 
+
+$key = q/a^Tx+s;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="58" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img32.png"
+ ALT="$a^Tx + s$">|; 
+
+$key = q/Bx=b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="54" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$Bx=b$">|; 
+
+$key = q/sumx_{i}^{2}ley^2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="76" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img29.png"
+ ALT="$\sum
+x_{i}^{2} \le y^2$">|; 
+
+$key = q/b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$b$">|; 
+
+$key = q/x;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$x$">|; 
+
+$key = q/mathrm{alpha};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="42" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img28.png"
+ ALT="$\mathrm{alpha}$">|; 
+
+$key = q/x^TQc,x+q^Txlemathrm{beta};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="152" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img26.png"
+ ALT="$x^TQc  x + q^Tx \le \mathrm{beta}$">|; 
+
+$key = q/A;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$A$">|; 
+
+$key = q/B^{-1}A;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="46" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$B^{-1} A$">|; 
+
+$key = q/(10X01^2+2X01*X02+2X02*X01+2X02^2)slash2;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="357" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img38.png"
+ ALT="$(10 X01^2 + 2 X01 * X02 + 2 X02 * X01 + 2 X02^2)/2$">|; 
+
+$key = q/AA^T;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="38" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img42.png"
+ ALT="$AA^T$">|; 
+
+$key = q/a^Ty-z=c;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="87" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img34.png"
+ ALT="$a^Ty - z = c$">|; 
+
+$key = q/j;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$j$">|; 
+
+$key = q/x^Txleyz;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="70" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$x^Tx \le y z$">|; 
+
+1;
+
